1741d028bb3e92c980654c2d99650179523c9f4b
Khandaker Mahfuz Hasan <khandakermahfuzhasan@gmail.com>
2020-06-19 16:26:40 +0600

    Finishing Touches State Variables and added README.md file

---
+++ README.md
@@ -0,0 +1,12 @@
+#Description: 
+Pig is a simple dice game which in its basic form is playable with just a single die. You win by being the first player to score 100 points.
+
+
+#GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1 or same rolled number three times then all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1 or same rolled number three times then all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
-let score, roundScore, activePlayer,sameDice,diceCount;
+let score, roundScore, activePlayer,sameDice,diceCount,gamePlaying;
 init();
 
 let diceDOM 	= document.querySelector('.dice');
@@ -18,96 +7,108 @@ let btnHoldDOM 	= document.querySelector('.btn-hold');
 let btnNewDOM 	= document.querySelector('.btn-new');
 //Event handler for btn-roll
 btnRollDOM.addEventListener('click',()=>{
-	btnHoldDOM.removeAttribute('disabled');
-	btnHoldDOM.style.opacity=1;
-	//1. Random number of dice
-	let dice 		= Math.floor(Math.random()*6)+1;;
-	//let dice 		= 6;
-	
-	//display the dice number/result
-	diceDOM.style.display='block';
-	diceDOM.src = `assets/images/dice-${dice}.png`;
-	//3. update the result when only the result/ rolled number was not 1
-	if(dice !==1){
-		
-		//check same Dice
-		if(dice !==sameDice){
-			//count roundScore
-			roundScore +=dice;
-			document.getElementById('current-'+activePlayer).textContent=roundScore;
-			sameDice=dice;
-			diceCount=1;
 
-		}else{
-			diceCount++;
-			if(diceCount !==3){
-				//count roundScore
-				roundScore +=dice;
-				document.getElementById('current-'+activePlayer).textContent=roundScore;
-				sameDice=dice;
+	if(gamePlaying){
+			btnHoldDOM.removeAttribute('disabled');
+			btnHoldDOM.style.opacity=1;
+			//1. Random number of dice
+			let dice 		= Math.floor(Math.random()*6)+1;;
+			//let dice 		= 6;
+			
+			//display the dice number/result
+			diceDOM.style.display='block';
+			diceDOM.src = `assets/images/dice-${dice}.png`;
+			//3. update the result when only the result/ rolled number was not 1
+			if(dice !==1){
+				
+				//check same Dice
+				if(dice !==sameDice){
+					//count roundScore
+					roundScore +=dice;
+					document.getElementById('current-'+activePlayer).textContent=roundScore;
+					sameDice=dice;
+					diceCount=1;
+
+				}else{
+					diceCount++;
+					if(diceCount !==3){
+						//count roundScore
+						roundScore +=dice;
+						document.getElementById('current-'+activePlayer).textContent=roundScore;
+						sameDice=dice;
+
+					}else{
+						console.log("same three");
+						//change to next player
+						nextPlayer();
+					}
+				}
 
 			}else{
-				console.log("same three");
 				//change to next player
 				nextPlayer();
+
 			}
-		}
-
-	}else{
-		//change to next player
-		nextPlayer();
-
-	}
-	console.log(dice, sameDice,diceCount);
+			console.log(dice, sameDice,diceCount);
+	}// close gamePlaying
 });
 
 // add another Event Listener for btn-hold
 btnHoldDOM.addEventListener('click',()=>{
-	//1. add current current Score to Global Score
-	score[activePlayer] +=roundScore;
-	document.getElementById('score-'+activePlayer).textContent = score[activePlayer];
 
-	//2. Update UI
+	if(gamePlaying){
+			//1. add current current Score to Global Score
+			score[activePlayer] +=roundScore;
+			document.getElementById('score-'+activePlayer).textContent = score[activePlayer];
+
+			//2. Update UI
 
 
-	//3. check if the player won the game
-	if(score[activePlayer] >=10){
-		diceDOM.setAttribute('src','assets/images/end-game.png');
-		diceDOM.style.border 	= '3px solid rgba(111, 206, 112, 0.74)';
-		btnRollDOM.setAttribute('disabled', 'disabled');
-		btnRollDOM.style.opacity=0.5;
-		btnHoldDOM.setAttribute('disabled', 'disabled');
-		btnHoldDOM.style.opacity=0.5;
-		document.getElementById('name-'+activePlayer).classList.add('activePoint');
-		document.getElementById('update-'+activePlayer).textContent = "Winner!";
-	}else{
-		//4. change player
-		nextPlayer();
+			//3. check if the player won the game
+			if(score[activePlayer] >=10){
+				diceDOM.setAttribute('src','assets/images/end-game.png');
+				diceDOM.style.border 	= '3px solid rgba(111, 206, 112, 0.74)';
+				btnRollDOM.setAttribute('disabled', 'disabled');
+				btnRollDOM.style.opacity=0.5;
+				btnHoldDOM.setAttribute('disabled', 'disabled');
+				btnHoldDOM.style.opacity=0.5;
+				document.getElementById('name-'+activePlayer).classList.add('activePoint');
+				document.getElementById('update-'+activePlayer).textContent = "Winner!";
+				gamePlaying = false;
+			}else{
+				//4. change player
+				nextPlayer();
 
-	}
+			}
+	}// close gamePlaying
+
+
 
 });
 
 //Event Listener for btn-new
 btnNewDOM.addEventListener('click',()=>{
 	init();
-	diceDOM.style.display 	='block';
-	diceDOM.classList.add('animation');
-	btnRollDOM.setAttribute('disabled','disabled');
-	btnRollDOM.style.opacity=0.5;
-	btnHoldDOM.setAttribute('disabled', 'disabled');
-	btnHoldDOM.style.opacity=0.5;
-	diceDOM.setAttribute('src','assets/images/start-game.png');
-	setTimeout(()=>{
-		diceDOM.style.display 	='none';
-		diceDOM.classList.remove('animation');
-		btnRollDOM.removeAttribute('disabled','disabled');
-		btnRollDOM.style.opacity=1;
-		btnHoldDOM.removeAttribute('disabled', 'disabled');
-		btnHoldDOM.style.opacity=1;
-	},1500);
-	document.querySelector('.player-0-panel').classList.add('active');
-	document.querySelector('.player-1-panel').classList.remove('active');
+	if(gamePlaying){
+		diceDOM.style.display 	='block';
+		diceDOM.classList.add('animation');
+		btnRollDOM.setAttribute('disabled','disabled');
+		btnRollDOM.style.opacity=0.5;
+		btnHoldDOM.setAttribute('disabled', 'disabled');
+		btnHoldDOM.style.opacity=0.5;
+		diceDOM.setAttribute('src','assets/images/start-game.png');
+		setTimeout(()=>{
+			diceDOM.style.display 	='none';
+			diceDOM.classList.remove('animation');
+			btnRollDOM.removeAttribute('disabled','disabled');
+			btnRollDOM.style.opacity=1;
+			btnHoldDOM.removeAttribute('disabled', 'disabled');
+			btnHoldDOM.style.opacity=1;
+		},1500);
+		document.querySelector('.player-0-panel').classList.add('active');
+		document.querySelector('.player-1-panel').classList.remove('active');
+
+	}// close gamePlaying
 });
 
 
@@ -127,6 +128,7 @@ function nextPlayer(){
 
 //initialize the application
 function init(){
+	gamePlaying		= true;
 	score 			= [0,0];
 	roundScore 		= 0;
 	activePlayer 	= 0;
